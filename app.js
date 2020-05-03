5f4a7434049c62c72cf4e5e8a09753c66c6d4a60
Jonathan Treffler <trefflerj@web.de>
2020-05-03 14:56:26 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const http = require('http');
+const url = require('url');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 const hostname = '0.0.0.0';
 const port = 8080;
 
+const cecDeviceId = 0;
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  let urlParts = url.parse(req.url);
+  let route = routes[parts.pathname];
+
+  if(route == "/shutdown") {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    exec("echo 'standby " + cecDeviceId + "' | cec-client -s -d 1").then(function() {
+      res.write("success");
+      res.end('');
+    });
+  } else {
+    res.statusCode = 400;
+    res.end('');
+  }
+
+  
 });
 
 server.listen(port, hostname, () => {
