d916ce182cdd71536094087499a3cfceb4184aad
Jonathan Treffler <mail@jonathan-treffler.de>
2020-07-17 19:08:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,15 @@
+#! /bin/sh
 apt update
 apt install -y cec-utils node npm
+
+chmod +x ./start.sh
+
+cp ./service /etc/init.d/HomeAssistantRemoteCEC
+
+chmod 755 /etc/init.d/HomeAssistantRemoteCEC
+
+update-rc.d HomeAssistantRemoteCEC defaults
+
+systemctl daemon-reload
+
+update-rc.d HomeAssistantRemoteCEC enable
